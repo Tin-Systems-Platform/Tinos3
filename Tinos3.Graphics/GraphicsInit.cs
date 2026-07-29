26ffc1d72e4a811317f9097f0ffc23942f0d38eb
randomusert <eetupajamaki@gmail.com>
2026-07-29 16:37:09 +0300

    Feature start: Graphics

---
+++ Tinos3.Graphics/GraphicsInit.cs
@@ -0,0 +1,10 @@
+﻿namespace Tinos3.Graphics
+{
+    public class GraphicsInit
+    {
+        public void initGraphics()
+        {
+            Console.WriteLine("GRAPHICS: Initializing graphics");
+        }
+    }
+}
